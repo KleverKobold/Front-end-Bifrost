561f50e25d71476f066d4a17def74c3bd03735d0
KleverKobold <104393766+KleverKobold@users.noreply.github.com>
2023-01-21 10:58:32 -0300

    Adding old PI structure

--- bifrost/src/App.js
+++ bifrost/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+// import LandingPage from "./components/LandingPage/LandingPage";
+// import Home from "./components/Home/Home";
+// import { GameCreate } from "./components/GameCreate/GameCreate";
+// import VideoGameDetail from "./components/VideoGameDetail/VideoGameDetail";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          {/* <Route exact path="/" component={LandingPage} />
+          <Route exact path="/home" component={Home} />
+          <Route path="/game" component={GameCreate} />
+          <Route exact path="/home/:id" component={VideoGameDetail} /> */}
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
